a8e42e9d528bd680997c6b0c06f9dcb5112ffe75
nbngoc123 <nbngoc.udu.ptit@gmail.com>
2026-06-08 17:37:41 +0700

    fix: add missing code-behind for AIToolsConfigPanel

---
+++ ClipboardAI/Views/Settings/AIToolsConfigPanel.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ClipboardAI.Views.Settings;
+
+public partial class AIToolsConfigPanel : UserControl
+{
+    public AIToolsConfigPanel()
+    {
+        InitializeComponent();
+    }
+}
